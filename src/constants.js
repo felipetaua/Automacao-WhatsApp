7b92d8d8c4bc0e2059578490395eeaeb09d06341
Tauã Felipe <127506090+felipetaua@users.noreply.github.com>
2024-12-12 08:40:02 -0300

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+/**
+ * Este projeto deve ser utilizado apenas em localhost
+ * para garantir que suas credenciais estarão seguras
+ * Caso queira utilizar em produção, recomendamos a utilização
+ * de variáveis de ambiente!
+ * 
+ * Para API funcionar é necessario os token e instancias
+ */
+const GZAPPY_API_TOKEN = "...";
+const GZAPPY_INSTANCE_ID = "...";
+const GZAPPY_URL = "https://api.gzappy.com/v1/message/send-message";
+
+export { GZAPPY_API_TOKEN, GZAPPY_INSTANCE_ID, GZAPPY_URL };
